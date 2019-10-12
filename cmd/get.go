061394c4e263b80a0bfe868977c76bcc4e0aaf88
chroju <chor.chroju@gmail.com>
2019-10-12 14:29:44 +0900

    Update get command print format

--- cmd/get.go
+++ cmd/get.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"github.com/spf13/cobra"
 	"github.com/chroju/para/ssmctl"
+	"github.com/fatih/color"
+	"text/tabwriter"
 )
 
 var (
@@ -15,6 +18,8 @@ var (
 		Short: "Get key value",
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			w := tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
+			query := args[0]
 			ssmManager, err := ssmctl.New()
 			if err != nil {
 				fmt.Println(err)
@@ -27,27 +32,35 @@ var (
 				}
 
 				for _, v := range resp {
-					if strings.Contains(*v.Name, args[0]) {
-						printValue(ssmManager, *v.Name)
+					index := strings.Index(*v.Name, query)
+					if index >= 0 {
+						resp, err := ssmManager.GetParameters(v)
+						if err != nil {
+							fmt.Println(err)
+						}
+						printValuesWithColor(w, ssmManager, *v.Name, *resp[0].Value, index, index+len(query))
 					}
 				}
 			} else {
-				printValue(ssmManager, args[0])
+				resp, err := ssmManager.GetParameters(query)
+				if err != nil {
+					fmt.Println(err)
+				}
+				printValue(w, query, *resp[0].Value)
 			}
 		},
 	}
 )
 
+func printValue(w *tabwriter.Writer, key string, value string) {
+	fmt.Fprintf(w, "%s\t%s\n", key, value)
+	w.Flush()
+}
 
-func printValue(s *ssmctl.SSMManager, key string) {
-	resp, err := s.GetParameters(key)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, v := range resp {
-		fmt.Printf("%v\n", *v.Value)
-	}
+func printValuesWithColor(w *tabwriter.Writer, s *ssmctl.SSMManager, key string, value string, begin int, end int) {
+	coloredKey := key[0:begin] + color.RedString(key[begin:end]) + key[end:]
+	printValue(w, coloredKey, value)
+	w.Flush()
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go v1.25.7
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/spf13/cobra v0.0.5
 )
