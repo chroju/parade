8e7c754c220d43763c69854e58f1d626802c6ab4
chroju <chroju@users.noreply.github.com>
2021-02-13 21:56:43 +0900

    Update version string to 0.2.0

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// VERSION is cli tool version
+const VERSION = "0.2.0"
+
 var (
 	// StdWriter is the io.Writer for standard output
 	StdWriter io.Writer
@@ -14,8 +17,9 @@ var (
 	ErrWriter io.Writer
 
 	rootCmd = &cobra.Command{
-		Use:   "parade",
-		Short: "simple SSM parameters CLI",
+		Use:     "parade",
+		Short:   "simple SSM parameters CLI",
+		Version: VERSION,
 		Long: `Parade is a simple CLI tool for AWS SSM parameter store.
 	Easy to read and writer key values in your parameter store.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
